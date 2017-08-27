e59b2dcee03d682dcef3b2807316b854426fc58b
Daniel Spargo <31382454+SaltyLiquorice@users.noreply.github.com>
2017-08-27 18:43:50 +1000

    Create TFT.ino

---
+++ TFT.ino
@@ -0,0 +1,21 @@
+//TALKING FOOD THERMOMETER
+//
+//Daniel Spargo, Malik Alyousfi, Azim Hanifa, Joseph Kannadan
+//FOR TADQ
+//
+//DESCRIPTION:
+//
+
+//I0 definitions
+
+//Internal state variables
+
+// PIN SETUP
+void setup() {
+
+}
+
+// MAIN LOOP
+void loop() {
+
+}
